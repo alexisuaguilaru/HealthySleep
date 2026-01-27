ee62ffb2839b502212e3a686bb73488bc705a06e
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-01-26 19:28:17 -0600

    MOD at rules of api service in prod to fix allow methods

--- ModelAPI/main.py
+++ ModelAPI/main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 origins = [
     'http://localhost',
     'http://localhost:8080',
-    'https://'+getenv('DOMAIN','project.com'),
+    getenv('DOMAIN','https://project.com'),
 ]
 app.add_middleware(
     CORSMiddleware,
